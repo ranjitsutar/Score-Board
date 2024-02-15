61d8cfacfce3dafc88552fd8aea4c0a5f7537f32
Ranjit Sutar <ranjitsutar>
2024-02-15 11:39:47 +0530

    Backend Config

---
+++ ScoreBoardBE/src/main/java/com/ranjit/scoreboardbe/ScoreBoardBeApplication.java
@@ -0,0 +1,13 @@
+package com.ranjit.scoreboardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreBoardBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreBoardBeApplication.class, args);
+	}
+
+}
---
+++ ScoreBoardBE/src/test/java/com/ranjit/scoreboardbe/ScoreBoardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranjit.scoreboardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreBoardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
