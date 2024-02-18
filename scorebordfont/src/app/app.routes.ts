fa5e7a5ec7807c0cef485c81d026d51857cf8e06
Ranjit Sutar <ranjitsutar>
2024-02-18 15:04:51 +0530

    Component are added

--- scorebordfont/src/app/app.routes.ts
+++ scorebordfont/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path : "history",
+    component : HistoryComponent
+},
+
+{
+    path : "home",
+    component : HomeComponent
+},
+{
+    path : "live",
+    component : LiveComponent
+},
+{
+    path : "point-table",
+    component : PointTableComponent
+},
+
+
+];
